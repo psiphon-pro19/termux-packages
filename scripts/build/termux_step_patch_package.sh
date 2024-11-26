a765c37ddd00fc2694e5ce7544b5cfe6efb0c66a
psiphon-pro19 <94272409+psiphon-pro19@users.noreply.github.com>
2024-11-26 23:26:41 +0700

    Update termux_step_patch_package.sh

--- scripts/build/termux_step_patch_package.sh
+++ scripts/build/termux_step_patch_package.sh
@@ -16,17 +16,5 @@ termux_step_patch_package() {
 	if [ "$TERMUX_ON_DEVICE_BUILD" = "true" ]; then
 		ON_DEVICE_PATCHES=$(find $TERMUX_PKG_BUILDER_DIR -mindepth 1 -maxdepth 1 -name \*.patch.ondevice | sort)
 	fi
-	shopt -s nullglob
-	for patch in $PATCHES $DEBUG_PATCHES $ON_DEVICE_PATCHES; do
-		echo "Applying patch: $(basename $patch)"
-		test -f "$patch" && sed \
-			-e "s%\@TERMUX_APP_PACKAGE\@%${TERMUX_APP_PACKAGE}%g" \
-			-e "s%\@TERMUX_BASE_DIR\@%${TERMUX_BASE_DIR}%g" \
-			-e "s%\@TERMUX_CACHE_DIR\@%${TERMUX_CACHE_DIR}%g" \
-			-e "s%\@TERMUX_HOME\@%${TERMUX_ANDROID_HOME}%g" \
-			-e "s%\@TERMUX_PREFIX\@%${TERMUX_PREFIX}%g" \
-			-e "s%\@TERMUX_PREFIX_CLASSICAL\@%${TERMUX_PREFIX_CLASSICAL}%g" \
-			"$patch" | patch --silent -Nbtp1
-	done
-	shopt -u nullglob
+
 }
