34895b8237b3fc85872ba515ca128580ad9bb276
psiphon-pro19 <94272409+psiphon-pro19@users.noreply.github.com>
2024-11-28 23:23:52 +0700

    Update build.sh CFLAGS

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -80,12 +80,12 @@ termux_step_host_build() {
 	pushd $PATCHELF_SRCDIR
 	./bootstrap.sh
 	./configure
-	make CFLAGS=" -Os -pthread " -j $TERMUX_PKG_MAKE_PROCESSES
+	make -j $TERMUX_PKG_MAKE_PROCESSES
 	popd
 
 	(cd "$TERMUX_PKG_SRCDIR" && ./buildconf --force)
-	"$TERMUX_PKG_SRCDIR/configure" ${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
-	make CFLAGS=" -Os -pthread " -j "$TERMUX_PKG_MAKE_PROCESSES"
+	"$TERMUX_PKG_SRCDIR/configure" CFLAGS=" -Os -pthread " ${TERMUX_PKG_EXTRA_HOSTBUILD_CONFIGURE_ARGS}
+	make -j "$TERMUX_PKG_MAKE_PROCESSES"
 }
 
 termux_step_pre_configure() {
