857acbf8c1aa8ebf3e746ab189d31870e1539b13
psiphon-pro19 <94272409+psiphon-pro19@users.noreply.github.com>
2024-12-20 00:55:15 +0700

    Update build.sh

--- packages/php/build.sh
+++ packages/php/build.sh
@@ -94,8 +94,8 @@ termux_step_pre_configure() {
 	local _libgcc_file="$($CC -print-libgcc-file-name)"
 	local _libgcc_path="$(dirname $_libgcc_file)"
 	local _libgcc_name="$(basename $_libgcc_file)"
-	LDFLAGS+=" -lresolv_wrapper -landroid-glob -llog -L$_libgcc_path -l:$_libgcc_name -lm -Wl,--hash-style=sysv "
-	CFLAGS+=" -Wl,--hash-style=both "
+	LDFLAGS+=" -lresolv_wrapper -landroid-glob -llog -L$_libgcc_path -l:$_libgcc_name -lm --hash-style=both "
+	#CFLAGS+=" -Wl,--hash-style=both "
 	export PATH=$PATH:$TERMUX_PKG_HOSTBUILD_DIR/sapi/cli/
 	export NATIVE_PHP_EXECUTABLE=$TERMUX_PKG_HOSTBUILD_DIR/sapi/cli/php
 	export NATIVE_MINILUA_EXECUTABLE=$TERMUX_PKG_HOSTBUILD_DIR/ext/opcache/jit/ir/minilua
